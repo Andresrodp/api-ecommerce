7484c209d0445c70ab3e9e9e9d716ee684138bbf
Andresrodp <arodriguezp.cs.1@gmail.com>
2024-07-12 20:20:57 -0500

    conexión a base de datos y primer modelo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Customer } from './domain/models/customers.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configModule: ConfigService) => ({
+        dialect: 'postgres',
+        host: configModule.get<string>('DB_HOST'),
+        port: configModule.get<number>('DB_PORT'),
+        username: configModule.get<string>('DB_USERNAME'),
+        password: configModule.get<string>('DB_PASSWORD'),
+        database: configModule.get<string>('DB_DATABASE'),
+        models: [Customer],
+        autoLoadModels: true,
+      }),
+      inject: [ConfigService],
+    }),
+    SequelizeModule.forFeature([Customer]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
